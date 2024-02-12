8372139aaacfc3fde2f5c0199922ddccb7accc6c
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2024-02-12 17:44:10 -0500

    test

--- test.py
+++ test.py
@@ -1,59 +1,22 @@
-import streamlit as st
-from docx import Document
-from io import StringIO
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.proxy import Proxy, ProxyType
-import time
-import logging
-import os
-import pandas as pd
+"""Scrapes quotes from the given url.
 
+The url is 'https://quotes.toscrape.com/'.
+"""
+
+import streamlit as st
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 import pandas as pd
+import time
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--window-size=1920x1080')
+chrome_options.add_argument('--disable-gpu')
 
-class LinkedInBot:
-    def __init__(self, delay=5):
-        if not os.path.exists("data"):
-            os.makedirs("data")
-        log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        logging.basicConfig(level=logging.INFO, format=log_fmt)
-        self.delay = delay
-        logging.info("Starting driver")
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--window-size=1920x1080')
-        chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-
-    def login(self, email, password):
-        """Go to LinkedIn and login"""
-        logging.info("Logging in")
-        self.driver.maximize_window()
-        self.driver.get('https://www.linkedin.com/login')
-        time.sleep(self.delay)
-
-        self.driver.find_element(By.ID, 'username').send_keys(email)
-        self.driver.find_element(By.ID, 'password').send_keys(password)
-
-        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
-        time.sleep(self.delay)
-
-def main():
-    st.set_page_config(layout="wide") 
-    st.title("LinkedIn Job Analysis")
-    if st.button("Scrape Jobs"):
-        bot = LinkedInBot()
-        bot.login('negi.m@northeastern.edu', 'Lonely123)')
-
-          
-if __name__ == "__main__":
-    main()
+url = 'https://google.com/'
+time.sleep(1000000)
+driver.get(url)
