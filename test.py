fb97bd14f9dbb0addad1434280b8b4f9cbd47451
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2024-02-12 17:22:22 -0500

    test

--- test.py
+++ test.py
@@ -1,17 +1,49 @@
 import streamlit as st
-import os, sys
-
-@st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
+from docx import Document
+from io import StringIO
+import pandas as pd
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+import time
+import logging
+import os
+import pandas as pd
 
-browser.get('http://google.com')
-st.write(browser.page_source)
+class LinkedInBot:
+    def __init__(self, delay=5):
+        if not os.path.exists("data"):
+            os.makedirs("data")
+        log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        logging.basicConfig(level=logging.INFO, format=log_fmt)
+        self.delay = delay
+        logging.info("Starting driver")
+        self.driver = webdriver.Chrome()
+
+    def login(self, email, password):
+        """Go to LinkedIn and login"""
+        logging.info("Logging in")
+        self.driver.maximize_window()
+        self.driver.get('https://www.linkedin.com/login')
+        time.sleep(self.delay)
+
+        self.driver.find_element(By.ID, 'username').send_keys(email)
+        self.driver.find_element(By.ID, 'password').send_keys(password)
+
+        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        time.sleep(self.delay)
+
+def main():
+    st.set_page_config(layout="wide") 
+    st.title("LinkedIn Job Analysis")
+    if st.button("Scrape Jobs"):
+        bot = LinkedInBot()
+        bot.login('negi.m@northeastern.edu', 'Lonely123)')
+
+          
+if __name__ == "__main__":
+    main()
